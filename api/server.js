6ff2c082d02e433b2b6d7b32d5ab03c348011381
NitithX374 <chorunrit@gmail.com>
2024-10-31 22:24:04 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
 const allowedOrigins = ['https://numer-pt-03-ex.vercel.app'];
 
