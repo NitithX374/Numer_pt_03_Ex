1106d266a285200aa21ffd4e740ae465bcc9533b
NitithX374 <chorunrit@gmail.com>
2024-10-31 20:23:00 +0700

    Update server.js

--- api/server.js
+++ api/server.js
@@ -2,18 +2,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
-
-// Middleware
-app.use(express.json());
 app.use(cors({
     origin: 'https://numer-pt-03-ex.vercel.app', // Replace with your frontend's URL
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
     optionsSuccessStatus: 200
 }));
+// Middleware
+app.use(express.json());
+
 // Connect to MongoDB
 const connectDB = async () => {
     try {
